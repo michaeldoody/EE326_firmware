46a9f8b395d8c2de2a7941de6185bae6a970be65
David-Lehman <davidlehman2021@u.northwestern.edu>
2020-02-28 15:46:59 -0600

    Updated camera.c and camera.h with the configure_twi, init_camera, and configure_camera functions and declarations.

--- webcam_firmware/src/camera.h
+++ webcam_firmware/src/camera.h
@@ -14,10 +14,32 @@
 #define OV7740_VSYNC_MASK			PIO_PA15
 #define OV7740_VSYNC_TYPE			PIO_PULLUP
 #define OV7740_DATA_BUS_PIO			PIOA
+#define OV_DATA_BUS_MASK            (0xFF000000UL)
+#define OV_DATA_BUS_PIO             PIOA
+#define OV_DATA_BUS_ID              ID_PIOA
+#define OV_POWER_PIO				PIOC
+#define OV_POWER_MASK				PIO_PC10
+
+
 
 
 #define CAP_DEST                    (0x60000000UL)
 
+#define ID_BOARD_TWI				ID_TWI0
+#define TWI_CLK						(400000UL)
+#define BOARD_TWI			        TWI0
+#define BOARD_TWI_IRQn		        TWI0_IRQn
 
+
+typedef struct twi_options {
+	//! MCK for TWI.
+	uint32_t master_clk;
+	//! The baud rate of the TWI bus.
+	uint32_t speed;
+	//! The desired address.
+	uint8_t chip;
+	//! SMBUS mode (set 1 to use SMBUS quick command, otherwise don't).
+	uint8_t smbus;
+} twi_options_t;
 
 #endif /* CAMERA_H_ */
