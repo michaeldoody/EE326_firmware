be4c7aa22570aac63f473630aa20c1518430ab7c
David-Lehman <davidlehman2021@u.northwestern.edu>
2020-03-02 14:07:05 -0600

    Changes to file structure and camera functions
    Added in OV2640.h/.c, timer_interface.h/.c, ov2640_tabe_registers.c. Updated functions and declarations in Camera.c and Camera.h. Added the find_image_len function to Camera.c

--- webcam_firmware/src/camera.c
+++ webcam_firmware/src/camera.c
@@ -13,7 +13,7 @@ uint16_t g_us_cap_rows = IMAGE_HEIGHT;
 static volatile uint32_t g_ul_vsync_flag = false;
 uint16_t g_us_cap_line = (IMAGE_WIDTH * 2);
 
-static void vsync_handler(uint32_t ul_id, uint32_t ul_mask)
+void vsync_handler(uint32_t ul_id, uint32_t ul_mask)
 {
 	unused(ul_id);
 	unused(ul_mask);
@@ -21,7 +21,7 @@ static void vsync_handler(uint32_t ul_id, uint32_t ul_mask)
 	g_ul_vsync_flag = true;
 }
 
-static void init_vsync_interrupts(void)
+void init_vsync_interrupts(void)
 {
 	/* Initialize PIO interrupt handler, see PIO definition in conf_board.h
 	**/
@@ -30,9 +30,11 @@ static void init_vsync_interrupts(void)
 
 	/* Enable PIO controller IRQs */
 	NVIC_EnableIRQ((IRQn_Type)OV7740_VSYNC_ID);
+	
+	pio_enable_interrupt(OV7740_VSYNC_PIO, OV7740_VSYNC_MASK)
 }
 
-static void configure_twi(void)
+void configure_twi(void)
 {
 	twi_options_t opt;
 	
@@ -54,7 +56,7 @@ static void configure_twi(void)
 
 
 
-static void pio_capture_init(Pio *p_pio, uint32_t ul_id)
+void pio_capture_init(Pio *p_pio, uint32_t ul_id)
 {
 	/* Enable peripheral clock */
 	pmc_enable_periph_clk(ul_id);
@@ -73,14 +75,15 @@ static void pio_capture_init(Pio *p_pio, uint32_t ul_id)
 	p_pio->PIO_PCMR &= ~((uint32_t)PIO_PCMR_ALWYS);
 	p_pio->PIO_PCMR &= ~((uint32_t)PIO_PCMR_HALFS);
 
-	#if !defined(DEFAULT_MODE_COLORED)
-	/* Samples only data with even index */
-	p_pio->PIO_PCMR |= PIO_PCMR_HALFS;
-	p_pio->PIO_PCMR &= ~((uint32_t)PIO_PCMR_FRSTS);
-	#endif
+// 	#if !defined(DEFAULT_MODE_COLORED)
+// 	/* Samples only data with even index */
+// 	p_pio->PIO_PCMR |= PIO_PCMR_HALFS;
+// 	p_pio->PIO_PCMR &= ~((uint32_t)PIO_PCMR_FRSTS);
+// 	#endif 
+
 }
 
-static uint8_t pio_capture_to_buffer(Pio *p_pio, uint8_t *uc_buf,
+uint8_t pio_capture_to_buffer(Pio *p_pio, uint8_t *uc_buf,
 uint32_t ul_size)
 {
 	/* Check if the first PDC bank is free */
@@ -98,86 +101,65 @@ uint32_t ul_size)
 	}
 }
 
-void ov_power(uint32_t ul_on_off, Pio* const p_pio, const uint32_t ul_mask)
+
+void init_camera(void)
 {
-	if (ul_on_off) {
-		pio_clear(p_pio, ul_mask);
-		} else {
-		pio_set(p_pio, ul_mask);
-	}
-}
-
-uint32_t twi_master_init(Twi *p_twi, const twi_options_t *p_opt)
-{
-	uint32_t status = TWI_SUCCESS;
-
-	/* Disable TWI interrupts */
-	p_twi->TWI_IDR = ~0UL;
-
-	/* Dummy read in status register */
-	p_twi->TWI_SR;
-
-	/* Reset TWI peripheral */
-	twi_reset(p_twi);
-
-	twi_enable_master_mode(p_twi);
-
-	/* Select the speed */
-	if (twi_set_speed(p_twi, p_opt->speed, p_opt->master_clk) == FAIL) {
-		/* The desired speed setting is rejected */
-		status = TWI_INVALID_ARGUMENT;
-	}
-
-	if (p_opt->smbus == 1) {
-		p_twi->TWI_CR = TWI_CR_QUICK;
-	}
-
-	return status;
-}
-
-
-
-static void init_camera(void)
-{
+	gpio_configure_pin(OV_RST_GPIO, OV_RST_FLAGS);
+	gpio_configure_pin(OV_HSYNC_GPIO, OV_HSYNC_FLAGS);
+	gpio_configure_pin(OV_VSYNC_MASK, OV7740_VSYNC_TYPE);
+	gpio_configure_pin(OV_DATA_BUS_D0, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D1, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D2, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D3, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D4, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D5, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D6, OV_DATA_BUS_FLAGS);
+	gpio_configure_pin(OV_DATA_BUS_D7, OV_DATA_BUS_FLAGS);
+	
+	pmc_enable_pllbck(7, 0x1, 1);
+	
+	
+		
 	/* Init Vsync handler*/
 	init_vsync_interrupts();
+	pio_disable_interrupt(OV7740_VSYNC_PIO, OV7740_VSYNC_MASK)
+	vsync_flag = false;
 
 	/* Init PIO capture*/
 	pio_capture_init(OV_DATA_BUS_PIO, OV_DATA_BUS_ID);
 
-	/* Turn on ov7740 image sensor using power pin */
-	ov_power(true, OV_POWER_PIO, OV_POWER_MASK);
 
-	/* Init PCK0 to work at 24 Mhz */
+
+	/* Init PCK1 to work at 24 Mhz */
 	/* 96/4=24 Mhz */
-	PMC->PMC_PCK[0] = (PMC_PCK_PRES_CLK_4 | PMC_PCK_CSS_PLLA_CLK);
+	PMC->PMC_PCK[0] = (PMC_PCK_PRES_CLK_4 | PMC_PCK_CSS_PLLB_CLK);
 	PMC->PMC_SCER = PMC_SCER_PCK0;
 	while (!(PMC->PMC_SCSR & PMC_SCSR_PCK0)) {
 	}
 	
+	configure_twi();
+	
 }
 
-static void configure_camera(void)
+void configure_camera(void)
 {
 	/* ov7740 Initialization */
 	while (ov_init(BOARD_TWI) == 1) {
 	}
 
 	/* ov7740 configuration */
-	ov_configure(BOARD_TWI, QVGA_YUV422_20FPS);
+	ov_configure(BOARD_TWI,JPEG_INIT);
+	ov_configure(BOARD_TWI, YUV422);
+	ov_configure(BOARD_TWI, JPEG);
+	ov_configure(BOARD_TWI, JPEG_320x240);
 
 	/* Wait 3 seconds to let the image sensor to adapt to environment */
 	delay_ms(3000);
 }
 
 
-static void start_capture(void)
+uint8_t start_capture(void)
 {
-	/* Set capturing destination address*/
-	g_p_uc_cap_dest_buf = (uint8_t *)CAP_DEST;
-
-	/* Set cap_rows value*/
-	g_us_cap_rows = IMAGE_HEIGHT;
 
 	/* Enable vsync interrupt*/
 	pio_enable_interrupt(OV7740_VSYNC_PIO, OV7740_VSYNC_MASK);
@@ -192,21 +174,47 @@ static void start_capture(void)
 	pio_disable_interrupt(OV7740_VSYNC_PIO, OV7740_VSYNC_MASK);
 
 	/* Enable pio capture*/
-	pio_capture_enable(OV7740_DATA_BUS_PIO);
+	pio_capture_enable(OV_DATA_BUS_PIO);
 
 	/* Capture data and send it to external SRAM memory thanks to PDC
 	 * feature */
-	pio_capture_to_buffer(OV7740_DATA_BUS_PIO, g_p_uc_cap_dest_buf,
-			(g_us_cap_line * g_us_cap_rows) >> 2);
+	pio_capture_to_buffer(OV_DATA_BUS_PIO, image_buffer,
+			IMAGE_MAX >> 2);
 
 	/* Wait end of capture*/
-	while (!((OV7740_DATA_BUS_PIO->PIO_PCISR & PIO_PCIMR_RXBUFF) ==
+	while (!((OV_DATA_BUS_PIO->PIO_PCISR & PIO_PCIMR_RXBUFF) ==
 			PIO_PCIMR_RXBUFF)) {
 	}
 
 	/* Disable pio capture*/
-	pio_capture_disable(OV7740_DATA_BUS_PIO);
+	pio_capture_disable(OV_DATA_BUS_PIO);
 
 	/* Reset vsync flag*/
 	g_ul_vsync_flag = false;
+	
+	uint8_t val = find_image_len();
+	return val;
+}
+
+
+uint8_t find_image_len(void){
+		uint8_t start_flag = 0;
+		uint32_t cPix = 0;
+		start_of_image = 0;
+		end_of_image = 0;
+		while( cPix+1 < IMAGE_MAX){
+			if(image_buffer[cPix] == 0xff && image_buffer[cPix+1] == 0xd8){
+				start_of_image = cPix;
+				start_flag = 1;
+			}
+			if(image_buffer[cPix] == 0xff && image_buffer[cPix+1] == 0xd9 && start_flag == 1){
+				image_end = cPix+2;
+				return 1;
+			}
+		}
+		start_of_image = 0;
+		image_end = 0;
+		
+		return 0;
+	}
 }
--- webcam_firmware/src/camera.h
+++ webcam_firmware/src/camera.h
@@ -13,23 +13,59 @@
 #define OV7740_VSYNC_ID				ID_PIOA
 #define OV7740_VSYNC_MASK			PIO_PA15
 #define OV7740_VSYNC_TYPE			PIO_PULLUP
-#define OV7740_DATA_BUS_PIO			PIOA
+#define OV_POWER_PIO				PIOC
+#define OV_VSYNC_MASK				PIO_PA15_IDX
+
+
+#define OV_RST_GPIO                 PIO_PA20_IDX
+#define OV_RST_FLAGS                (PIO_OUTPUT_1 | PIO_DEFAULT)
+#define OV_RST_MASK                 PIO_PA20
+#define OV_RST_PIO                  PIOA
+#define OV_RST_ID                   ID_PIOA
+#define OV_RST_TYPE                 PIO_OUTPUT_1
+
+/** OV_HSYNC pin definition */
+#define OV_HSYNC_GPIO               PIO_PA16_IDX
+#define OV_HSYNC_FLAGS              (PIO_PULLUP | PIO_IT_RISE_EDGE)
+#define OV_HSYNC_MASK               PIO_PA16
+#define OV_HSYNC_PIO                PIOA
+#define OV_HSYNC_ID                 ID_PIOA
+#define OV_HSYNC_TYPE				IO_PULLUP
+
+
+
+/** OV Data Bus pins */
+#define OV_DATA_BUS_D0              PIO_PA24_IDX
+#define OV_DATA_BUS_D1              PIO_PA25_IDX
+#define OV_DATA_BUS_D2              PIO_PA26_IDX
+#define OV_DATA_BUS_D3              PIO_PA27_IDX
+#define OV_DATA_BUS_D4              PIO_PA28_IDX
+#define OV_DATA_BUS_D5              PIO_PA29_IDX
+#define OV_DATA_BUS_                PIO_PA30_IDX
+#define OV_DATA_BUS_D7              PIO_PA31_IDX
+#define OV_DATA_BUS_FLAGS           (PIO_INPUT | PIO_PULLUP)
 #define OV_DATA_BUS_MASK            (0xFF000000UL)
 #define OV_DATA_BUS_PIO             PIOA
 #define OV_DATA_BUS_ID              ID_PIOA
-#define OV_POWER_PIO				PIOC
-#define OV_POWER_MASK				PIO_PC10
+#define OV_DATA_BUS_TYPE			PIO_INPUT
+#define OV_DATA_BUS_ATTR			PIO_DEFAULT
 
 
 
-
-#define CAP_DEST                    (0x60000000UL)
+#define CAP_DEST					(0x60000000UL)
 
 #define ID_BOARD_TWI				ID_TWI0
 #define TWI_CLK						(400000UL)
 #define BOARD_TWI			        TWI0
 #define BOARD_TWI_IRQn		        TWI0_IRQn
 
+
+#define IMAGE_MAX				50000
+uint8_t image_buffer[MAX_IMAGE_SIZE];
+
+volatile uint32_t start_of_image;
+volatile uint32_t end_of_image;
+volatile uint32_t vsync_flag;
 
 typedef struct twi_options {
 	//! MCK for TWI.
@@ -41,5 +77,18 @@ typedef struct twi_options {
 	//! SMBUS mode (set 1 to use SMBUS quick command, otherwise don't).
 	uint8_t smbus;
 } twi_options_t;
+
+
+
+
+void vsync_handler(uint32_t ul_id, uint32_t ul_mask);
+void init_vsync_interrupts(void);
+void configure_twi(void);
+void pio_capture_init(Pio *p_pio, uint32_t ul_id);
+void init_camera(void);
+uint8_t start_capture(void);
+uint8_t find_image_len(void);
+
+
 
 #endif /* CAMERA_H_ */
